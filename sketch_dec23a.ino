806ebd914e4f51f71cb9ed59b37d1ea3fcb1ca7f
superpowerman176 <38111705+superpowerman176@users.noreply.github.com>
2018-12-23 11:26:37 +0100

    Add test Arduino sketch

---
+++ sketch_dec23a.ino
@@ -0,0 +1,14 @@
+int incomingByte = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()){
+    incomingByte = Serial.read();
+
+    Serial.print("recieved: ");
+    Serial.println(incomingByte, DEC);
+  }
+}
