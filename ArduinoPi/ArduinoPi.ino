6b3763f04feb4d2c32ed647bac14038f30a1633b
superpowerman176 <38111705+superpowerman176@users.noreply.github.com>
2019-03-31 14:18:02 +0200

    Refactoring
    not tested!

--- ArduinoPi/ArduinoPi.ino
+++ ArduinoPi/ArduinoPi.ino
@@ -3,7 +3,6 @@
 
 unsigned char message[TOTAL_MSG_LENGTH+1];
 int messageLength = 0;
-bool start = false;
 
 MotorManager* motor_manager = new MotorManager;
   
@@ -17,38 +16,45 @@ void setup(){
  
 void loop(){
     if (Serial.available() > 0){
-        unsigned char receivedByte = Serial.read();
-        if(receivedByte){
-          buildMessage(receivedByte);
-        }
+      readOnSerial();
     }
 }
 
-void buildMessage(unsigned char receivedByte){
-  
-  if(charNotStart(receivedByte))
-    return;
+void readOnSerial(){
+  unsigned char receivedByte = Serial.read();
+  if(receivedByte){
+    buildMessage(receivedByte);
+    interpretCompleteMessage();
+  }
+}
 
-  message[messageLength] = receivedByte;
-  messageLength++;    
-  if(messageLength == TOTAL_MSG_LENGTH){
+void buildMessage(unsigned char receivedByte){
+  checkMessageStart(receivedByte);
+  if(messageLength > 0){
+    addToMessage(receivedByte);
+  }
+}
+
+void checkMessageStart(unsigned char receivedByte){
+  if(receivedByte == MSG_START_SEQ){
+    resetMessage();
+    addToMessage(receivedByte);
+  }
+}
+
+void resetMessage(){
+  messageLength = 0;
+}
+
+void addToMessage(unsigned char byte){
+  message[messageLength] = byte;
+  messageLength++;   
+}
+
+void interpretCompleteMessage(){
+    if(messageLength == TOTAL_MSG_LENGTH){
     interpret(&message[0], motor_manager);
     resetMessage();
   }  
 }
 
-int charNotStart(unsigned char receivedByte){
-  if(receivedByte == MSG_START_SEQ){
-    resetMessage();
-    start = true;
-  }
-  if(!start)
-    return -1;
-
-  return 0;
-}
-
-void resetMessage(){
-  messageLength = 0;
-  start = false;
-}
