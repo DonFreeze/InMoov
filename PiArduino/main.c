7438ea0391fcd843369db862cf22b8a46a8437c6
spm176 <be-felix@web.de>
2019-04-03 16:31:14 +0200

    fixed message input: The message input is now filled up with zeros

--- PiArduino/main.c
+++ PiArduino/main.c
@@ -2,9 +2,9 @@
 
 void showHelp(char*);
 void initStandardMessage(char*);
+int validateMessage(char*);
 
 int main(int argc, char* argv[]){
-
     extern char *optarg;
     int option;
 
@@ -18,33 +18,23 @@ int main(int argc, char* argv[]){
     while((option = getopt(argc, argv, "m:s:b:p:hv")) != -1){
 	switch(option){   
         case 'm':
-            if(strlen(optarg)%2 != 0){
-                printf("Only straight values allowed for message\n");
+            if(validateMessage(optarg) < 0)
                 return -1;
-            }
-            if(strlen(optarg)/2 +1 > messageLength){
-                printf("Message to Long\n");
-                return -1;
-            }
 
-            // Build message String
-            unsigned long value = strtoul( optarg, NULL, 16); 
+            unsigned long value = strtoul(optarg, NULL, 16); 
             for(int i=messageLength-1; i>0; i--)
             {
                 message[i] = value & 0xFF;
                 value >>= 8;            
             }
+            break;
 
-            if(verify){
-                for(int i=0; i<messageLength;i++)
-                    printf("message[%d]: %d\n",i,message[i]);
-            }
-            break;	 
         case 'b':
             baud = atoi(optarg);
             if(verify)
                 printf("baud: %d\n",baud);
             break;
+
         case 'p':
             port = (char*) malloc(6 + strlen(optarg));
             strncat(port, "/dev/", 6);
@@ -53,18 +43,25 @@ int main(int argc, char* argv[]){
             if(verify)
                 printf("port: %s\n",port);
             break;
+
         case 'v':
             verify = 1;
             break;
+
         case 'h':
             showHelp(argv[0]);
             return 0;
+
         case '?':
             printf("Undefined parameter: Try '%s -h' for more information\n", argv[0]);
             return -3;
         }
     }
 
+    if(verify){
+        for(int i=0; i<messageLength;i++)
+            printf("message[%d]: %d\n",i,message[i]);
+    }
 
     int uart = setupUART(baud, port);
     if(uart < 0){
@@ -82,9 +79,21 @@ int main(int argc, char* argv[]){
     return messageReturn;
 }
 
-void initStandardMessage(char *message){
-    message[0] = MSG_SRT;
-    message[1] = 0x10;
+void initStandardMessage(char *msg){
+    memset(msg, 0, messageLength);
+    msg[0] = MSG_SRT;
+    msg[1] = 0x10;
+}
+
+int validateMessage(char *msg){
+    while(strlen(msg) < (messageLength-1)*2){   //two input hex is one char
+        strncat(msg, "0", 1);
+    }
+    if(strlen(msg) > (messageLength-1)*2){
+        printf("Message to Long\n");
+        return -1;
+    }
+    return 1;
 }
 
 void showHelp(char* progName){
