eea13e2e0003046c3651a3f721b0d1ddf990d95d
spm176 <be-felix@web.de>
2019-04-03 18:43:02 +0200

    rename and update to new communication start char

---
+++ ArduinoPi/Interpreter.cpp
@@ -0,0 +1,44 @@
+#include "Interpreter.h"
+
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+unsigned char *msg;
+
+// TODO create a List of all Components
+// Componend List has instances like motor and led
+// Create an Interpreter Object and do that in the Konstruktor?
+
+void Interpreter::interpret(unsigned char* message, MotorManager* mm_ptr){
+    msg = message;
+
+    if(messageIsValid()){
+      // Do stuff
+    }
+}
+
+bool Interpreter::messageIsValid(){
+  // Conponend bigger than List index? error
+  // To big values? Is it posible to validate that?
+  
+  // Example Code!
+  if(msg[1] > 0x30){
+    buildErrorMessage(ERROR_COMPONEND);
+    return false;
+  }
+  if(msg[2] > 0x10){
+    buildErrorMessage(ERROR_OPTYPE);
+    return false;
+  }
+  return true;
+}
+
+void Interpreter::buildErrorMessage(unsigned char errorType){
+  msg[2] = ERROR_ASW;
+  msg[3] = errorType;
+  msg[4] = '\0';
+}
---
+++ ArduinoPi/Interpreter.h
@@ -0,0 +1,40 @@
+/* Robo Project
+ * Arduino Interpreter for the Raspberry Pi commands
+ * Commands are specified in ReadMe file
+ */
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "MotorManager.h"
+
+#define TOTAL_MSG_LENGTH 7
+#define MSG_START_SEQ   0xFE
+
+#define STATUS  0x10
+#define EYE     0x20
+#define MOTOR   0x30
+#define WRITE   0x01
+#define READ    0x02
+
+
+// ERROR Codes
+#define ERROR_ASW             'F'
+#define ERROR_MSG_LENGTH      0x01
+#define ERROR_COMPONEND       0x02
+#define ERROR_OPTYPE          0x03
+
+class Interpreter
+{
+public:
+    static void interpret(unsigned char*, MotorManager* mm_ptr);
+
+private:
+    static bool messageIsValid();
+    static void buildErrorMessage(unsigned char);
+
+};
+
+
+
+
+#endif
